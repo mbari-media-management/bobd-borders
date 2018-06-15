abdaef2c6d110e296c0c6af8d296ca0fc8122ef9
Kevin Barnard <kbarnard@mbari.org>
2018-06-15 11:46:45 -0700

    Add build, Intellij project config

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 mvn clean compile assembly:single
-mv target/bobd-borders-1.0.0.jar bobd-borders.jar
-rm target/bobd-borders-1.0.0-jar-with-dependencies.jar
+cp target/bobd-borders-1.0.0.jar bobd-borders.jar
